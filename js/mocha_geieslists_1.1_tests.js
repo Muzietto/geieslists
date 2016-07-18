1b2a8c247743edb054395acd80ae586d48a01a2f
Muzietto <marco_faustinelli@yahoo.com>
2016-07-18 18:54:38 +0100

    minor stuff

--- js/mocha_geieslists_1.1_tests.js
+++ js/mocha_geieslists_1.1_tests.js
@@ -18,29 +18,29 @@ describe('Implementing SICP chapter 2 using Geieslists requires', function () {
     expect(L.foldl((acc,x)=>acc+x,0,L.ArrayToList([1,3,6]))).to.be.equal(10);
 
   });
-  
+
   it('a function to build set as trees in a naive manner', function() {
-    
+
     expect(L.build_set_naive(L.ArrayToList(['a'])).c).to.be.equal('[a,[],[]]');
     expect(L.build_set_naive(L.ArrayToList(['a','b'])).c).to.be.equal('[a,[],[b,[],[]]]');
     expect(L.build_set_naive(L.ArrayToList(['b','a'])).c).to.be.equal('[b,[a,[],[]],[]]');
 
   });
-  
+
   it('a function element_of_set', function() {
-    
-    
+
+
 
     expect(L.fold((x,ys)=>x+ys,0,L.ArrayToList([1,3,5]))).to.be.equal(9);
     expect(L.foldl((acc,x)=>acc+x,0,L.ArrayToList([1,3,6]))).to.be.equal(10);
 
   });
-  
+
   it('a function to build set as balanced trees', function() {
-    
+
     expect(L.fold((x,ys)=>x+ys,0,L.ArrayToList([1,3,5]))).to.be.equal(9);
     expect(L.foldl((acc,x)=>acc+x,0,L.ArrayToList([1,3,6]))).to.be.equal(10);
 
   });
-  
+
 });
