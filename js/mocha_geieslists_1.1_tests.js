a639aee7f8b6c0cf1b9b1525f084059d762e74e8
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-07-19 11:18:40 +0100

    Update mocha_geieslists_1.1_tests.js

--- js/mocha_geieslists_1.1_tests.js
+++ js/mocha_geieslists_1.1_tests.js
@@ -19,25 +19,34 @@ describe('Implementing SICP chapter 2 using Geieslists requires', function () {
 
   });
 
-  it('a function to build set as trees in a naive manner', function() {
+  it('a function to build sets as trees in a naive manner', function() {
 
     expect(L.build_set_naive(L.ArrayToList(['a'])).c).to.be.equal('[a,[],[]]');
+    expect(L.build_set_naive(L.ArrayToList(['a','a'])).c).to.be.equal('[a,[],[]]');
     expect(L.build_set_naive(L.ArrayToList(['a','b'])).c).to.be.equal('[a,[],[b,[],[]]]');
+    expect(L.build_set_naive(L.ArrayToList(['a','a','b'])).c).to.be.equal('[a,[],[b,[],[]]]');
+    expect(L.build_set_naive(L.ArrayToList(['a','b','b'])).c).to.be.equal('[a,[],[b,[],[]]]');
+    expect(L.build_set_naive(L.ArrayToList(['a','b','a'])).c).to.be.equal('[a,[],[b,[],[]]]');
+    expect(L.build_set_naive(L.ArrayToList(['a','b','b','a'])).c).to.be.equal('[a,[],[b,[],[]]]');
     expect(L.build_set_naive(L.ArrayToList(['b','a'])).c).to.be.equal('[b,[a,[],[]],[]]');
+    expect(L.build_set_naive(L.ArrayToList(['b','a','a','b','b','a'])).c).to.be.equal('[b,[a,[],[]],[]]');
+    expect(L.build_set_naive(L.ArrayToList(['m','a','r','c'])).c).to.be.equal('[m,[a,[],[c,[],[]]],[r,[],[]]]');
+    expect(L.build_set_naive(
+	  L.ArrayToList(['s','u','b','d','e','r','m','a','t','o','g','l','y','p','h','i','c'])).c).to.be.equal(
+	  '[s,[b,[a,[],[]],[d,[c,[],[]],[e,[],[r,[m,[g,[],[l,[h,[],[i,[],[]]],[]]],[o,[],[p,[],[]]]],[]]]]],[u,[t,[],[]],[y,[],[]]]]');
 
   });
 
   it('a function element_of_set', function() {
-
-
-
-    expect(L.fold((x,ys)=>x+ys,0,L.ArrayToList([1,3,5]))).to.be.equal(9);
-    expect(L.foldl((acc,x)=>acc+x,0,L.ArrayToList([1,3,6]))).to.be.equal(10);
-
+    // NB next tree must be ordered!
+    var tree = L.make_tree('m',L.make_tree('a',L.nil,L.make_tree('r',L.make_tree('c',L.nil,L.nil),L.nil)),L.nil);
+    expect(L.element_of_set('c',tree)).to.be.ok;
+    expect(L.element_of_set('b',tree)).to.be.not.ok;
   });
 
   it('a function to build set as balanced trees', function() {
 
+    // TODO - implement me
     expect(L.fold((x,ys)=>x+ys,0,L.ArrayToList([1,3,5]))).to.be.equal(9);
     expect(L.foldl((acc,x)=>acc+x,0,L.ArrayToList([1,3,6]))).to.be.equal(10);
 
